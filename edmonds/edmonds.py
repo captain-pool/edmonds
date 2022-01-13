36bc4f36600ba6bb4db77560977771db35cfa92e
Adrish Dey <adrishd.cse2017@nsec.ac.in>
2022-01-13 19:38:59 +0530

    adding support to py3. linting

--- edmonds/edmonds.py
+++ edmonds/edmonds.py
@@ -1,6 +1,9 @@
+from __future__ import print_function
+
 import sys
 
 # --------------------------------------------------------------------------------- #
+
 
 def _input(filename):
     prices = {}
@@ -8,36 +11,39 @@ def _input(filename):
 
     for line in file(filename).readlines():
         (name, src, dst, price) = line.rstrip().split()
-        name = int(name.replace('M',''))
-        src = int(src.replace('C',''))
-        dst = int(dst.replace('C',''))
+        name = int(name.replace("M", ""))
+        src = int(src.replace("C", ""))
+        dst = int(dst.replace("C", ""))
         price = int(price)
-        t = (src,dst)
+        t = (src, dst)
         if t in prices and prices[t] <= price:
             continue
         prices[t] = price
         names[t] = name
 
-    return prices,names
+    return prices, names
 
-def _load(arcs,weights):
+
+def _load(arcs, weights):
     g = {}
-    for (src,dst) in arcs:
+    for (src, dst) in arcs:
         if src in g:
-            g[src][dst] = weights[(src,dst)]
+            g[src][dst] = weights[(src, dst)]
         else:
-            g[src] = { dst : weights[(src,dst)] }
+            g[src] = {dst: weights[(src, dst)]}
     return g
+
 
 def _reverse(graph):
     r = {}
     for src in graph:
-        for (dst,c) in graph[src].items():
+        for (dst, c) in graph[src].items():
             if dst in r:
                 r[dst][src] = c
             else:
-                r[dst] = { src : c }
+                r[dst] = {src: c}
     return r
+
 
 def _getCycle(n, g, visited=None, cycle=None):
     if visited is None:
@@ -50,10 +56,11 @@ def _getCycle(n, g, visited=None, cycle=None):
         return cycle
     for e in g[n]:
         if e not in visited:
-            cycle = _getCycle(e,g,visited,cycle)
+            cycle = _getCycle(e, g, visited, cycle)
     return cycle
 
-def _mergeCycles(cycle,G,RG,g,rg):
+
+def _mergeCycles(cycle, G, RG, g, rg):
     allInEdges = []
     minInternal = None
     minInternalWeight = sys.maxint
@@ -63,42 +70,44 @@ def _mergeCycles(cycle,G,RG,g,rg):
         for e in RG[n]:
             if e in cycle:
                 if minInternal is None or RG[n][e] < minInternalWeight:
-                    minInternal = (n,e)
+                    minInternal = (n, e)
                     minInternalWeight = RG[n][e]
                     continue
             else:
-                allInEdges.append((n,e))        
+                allInEdges.append((n, e))
 
     # find the incoming edge with minimum modified cost
     minExternal = None
     minModifiedWeight = 0
-    for s,t in allInEdges:
-        u,v = rg[s].popitem()
+    for s, t in allInEdges:
+        u, v = rg[s].popitem()
         rg[s][u] = v
         w = RG[s][t] - (v - minInternalWeight)
         if minExternal is None or minModifiedWeight > w:
-            minExternal = (s,t)
+            minExternal = (s, t)
             minModifiedWeight = w
 
-    u,w = rg[minExternal[0]].popitem()
-    rem = (minExternal[0],u)
+    u, w = rg[minExternal[0]].popitem()
+    rem = (minExternal[0], u)
     rg[minExternal[0]].clear()
     if minExternal[1] in rg:
         rg[minExternal[1]][minExternal[0]] = w
     else:
-        rg[minExternal[1]] = { minExternal[0] : w }
+        rg[minExternal[1]] = {minExternal[0]: w}
     if rem[1] in g:
         if rem[0] in g[rem[1]]:
             del g[rem[1]][rem[0]]
     if minExternal[1] in g:
         g[minExternal[1]][minExternal[0]] = w
     else:
-        g[minExternal[1]] = { minExternal[0] : w }
+        g[minExternal[1]] = {minExternal[0]: w}
+
 
 # --------------------------------------------------------------------------------- #
 
-def mst(root,G):
-    """ The Chu-Lui/Edmond's algorithm
+
+def mst(root, G):
+    """The Chu-Lui/Edmond's algorithm
 
     arguments:
 
@@ -146,21 +155,21 @@ def mst(root,G):
         if len(RG[n]) == 0:
             continue
         minimum = sys.maxint
-        s,d = None,None
+        s, d = None, None
         for e in RG[n]:
             if RG[n][e] < minimum:
                 minimum = RG[n][e]
-                s,d = n,e
+                s, d = n, e
         if d in g:
             g[d][s] = RG[s][d]
         else:
-            g[d] = { s : RG[s][d] }
-            
+            g[d] = {s: RG[s][d]}
+
     cycles = []
     visited = set()
     for n in g:
         if n not in visited:
-            cycle = _getCycle(n,g,visited)
+            cycle = _getCycle(n, g, visited)
             cycles.append(cycle)
 
     rg = _reverse(g)
@@ -171,6 +180,7 @@ def mst(root,G):
 
     return g
 
+
 # --------------------------------------------------------------------------------- #
 
 if __name__ == "__main__":
@@ -178,13 +188,13 @@ if __name__ == "__main__":
         filename = sys.argv[1]
         root = sys.argv[2]
     except IndexError:
-        sys.stderr.write('no input and/or root node specified\n')
-        sys.stderr.write('usage: python edmonds.py <file> <root>\n')
+        sys.stderr.write("no input and/or root node specified\n")
+        sys.stderr.write("usage: python edmonds.py <file> <root>\n")
         sys.exit(1)
 
-    prices,names = _input(filename)
-    g = _load(prices,prices)
-    h = mst(int(root),g)
+    prices, names = _input(filename)
+    g = _load(prices, prices)
+    h = mst(int(root), g)
     for s in h:
         for t in h[s]:
-            print "%d-%d" % (s,t)
+            print("%d-%d" % (s, t))
